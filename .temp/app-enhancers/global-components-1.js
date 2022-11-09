2cccd6ea7ada5d7c8ea39b57eea1a22748520b38
fdliu <493884221@qq.com>
2022-11-09 16:32:15 +0800

    update-1109

--- .temp/app-enhancers/global-components-1.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-Vue.component("Badge", () => import("d:\\xingkong\\vuepress-theme-xing\\theme-vdoing\\global-components\\Badge"))
-Vue.component("CodeGroup", () => import("d:\\xingkong\\vuepress-theme-xing\\theme-vdoing\\global-components\\CodeGroup"))
-Vue.component("CodeBlock", () => import("d:\\xingkong\\vuepress-theme-xing\\theme-vdoing\\global-components\\CodeBlock"))
-
-
-export default {}
